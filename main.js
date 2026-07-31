2b0f968a3a35a15a002102becb4a37d25c70afb3
Michael Emil Lamei Fawzy <Michaellemill2009@gmail.com>
2026-07-31 19:38:52 +0300

    Add files via upload
    added the two other pyramids, fixed some errors (forgot to add semicolons for some lines and forgot add sun light to the scene). Note: AI helped me in the sizing, position, and color values to provide the most realistic version of the scene.

--- main.js
+++ main.js
@@ -25,11 +25,24 @@ animate();
 const khufuGeomertry = new THREE.ConeGeometry(4,5,4)
 const khufuMaterial = new THREE.MeshStandardMaterial({color: 0xc2a878});
 const khufu = new THREE.Mesh(khufuGeomertry, khufuMaterial);
-khufu.position.set(0, 2.5, 0)
-scene.add(khufu)
+khufu.position.set(0, 2.5, 0);
+scene.add(khufu);
+
+const khafreGeomertry = new THREE.ConeGeometry(3.7,4.65,4)
+const khafreMaterial = new THREE.MeshStandardMaterial({color: 0xb89a72});
+const khafre = new THREE.Mesh(khafreGeomertry, khafreMaterial);
+khafre.position.set(-9, 2.325, 2);
+scene.add(khafre);
+
+const menkaureGeomertry = new THREE.ConeGeometry(3.7,4.65,4)
+const menkaureMaterial = new THREE.MeshStandardMaterial({color: 0xaf8f66});
+const menkaure = new THREE.Mesh(menkaureGeomertry, menkaureMaterial);
+menkaure.position.set(-9, 2.325, 2);
+scene.add(menkaure);
 
 const ambientlight = new THREE.AmbientLight(0xffffff, 0.4);
 scene.add(ambientlight);
 
 const sunlight = new THREE.Directedlight(0xfff2d9, 1.2);
 sunlight.position.set(10, 15, 10);
+scene.add(sunlight);
